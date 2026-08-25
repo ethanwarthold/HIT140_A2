f14eef11c35d62571e3f169802494c6e7ee4a8e9
Erik <99470600+ErikSando@users.noreply.github.com>
2026-08-25 16:56:42 +0930

    Two sample t-test
    Found a statistically significant difference

--- ErikSando/main.py
+++ ErikSando/main.py
@@ -1,5 +1,6 @@
 import math
 import pandas as pd
+import scipy.stats as st
 import statsmodels.stats.weightstats as stm
 
 sample_mf = pd.read_csv("data/sample_mf.csv")
@@ -58,3 +59,20 @@ half_interval_fw = (ci_fw_upper - ci_fw_lower) / 2
 print()
 print(f"Midfielders  {C}% CI:  {mean_mf_pop:.2f} ± {half_interval_mf:.2f}  :  {ci_mf_lower:.2f} to {ci_mf_upper:.2f}")
 print(f"Forwards     {C}% CI:  {mean_fw_pop:.2f} ± {half_interval_fw:.2f}  :  {ci_fw_lower:.2f} to {ci_fw_upper:.2f}")
+
+# two sample t-test
+# null hypothesis: same accuracy between mf and fw, mean_mf = mean_fw
+# alternative hypothesis: mean_mf != mean_fw (two sided test)
+
+t_stats, p_value = st.ttest_ind_from_stats(
+    mean_mf, stddev_mf, n_mf, mean_fw, stddev_fw, n_fw,
+    equal_var=False, alternative="two-sided"
+)
+
+print(f"\nt* = {t_stats:.4f}\np = {p_value:.4f}")
+
+if p_value < 0.05:
+    print("\nNull hypothesis is rejected, there is a statisically significant difference.")
+
+else:
+    print("Null hypothesis can not be rejected, there is not a statistically significant difference.")
