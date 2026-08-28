483bc35666beba42dfad3c38ecf821c5caa937bb
Erik <99470600+ErikSando@users.noreply.github.com>
2026-08-28 17:27:36 +0930

    Added some more comments

--- ErikSando/take_sample.py
+++ ErikSando/take_sample.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# Data wrangling, preparation, and sampling
+
 df = pd.read_csv("data/fifa-player-dist.csv")
 
 # Filter out players with <10 attempted passes
@@ -11,9 +13,11 @@ df = df[["Player", "Position", "Passing Accuracy (%)"]]
 df_mf = df[df["Position"] == "MF"] # Midfield players
 df_fw = df[df["Position"] == "FW"] # Forward players
 
+RANDOM_STATE = 12345
+
 # Take random samples with n = 30
-sample_mf = df_mf.sample(n=30, random_state=12345)
-sample_fw = df_fw.sample(n=30, random_state=12345)
+sample_mf = df_mf.sample(n=30, random_state=RANDOM_STATE)
+sample_fw = df_fw.sample(n=30, random_state=RANDOM_STATE)
 
 # Write samples to a CSV file
 sample_mf.to_csv("data/sample_mf.csv", index=False)
